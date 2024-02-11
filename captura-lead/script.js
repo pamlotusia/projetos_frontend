a518e5cfd15023e0526e18435b99be174a18acaa
Pamela Lima <pamela.jesus.lima@gmail.com>
2024-02-11 10:02:34 -0300

    refinando estilo

--- captura-lead/script.js
+++ captura-lead/script.js
@@ -2,35 +2,42 @@
 // Para a validar o email é legal usar uma expressão regular.
 // Tente colocar todas as validações em uma função só, disparada a partir do click do botão.
 
-function noEmpthy(e) {
-  let text 
-  if (e.target.value == '') {
-    text = `${e.target.name} cannot be empty`
-    console.log(text)
-  }
-  negativeMessage(e, text)
-}
+function validateInfos(e) {
+  e.preventDefault()
 
-function valideEmail() {
+  // tratando email primeiro
   let email = document.querySelector('#email').value
+  let divParent = document.querySelector('#email').parentElement
+  let message = document.createElement('p')
+  message.classList.add('message')
+  let text
+
   const regexp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
   const test = regexp.test(email)
   if (!test) {
-    console.log(`Looks like this is not an email`)
+    text = 'Looks like this is not an email'
+    message.innerText = text
+    divParent.appendChild(message)
   }
+
+  // tratando qualquer input que possa estar não preenchido
+  let inputs = document.querySelectorAll('input')
+
+  inputs.forEach((input)=>{
+    if(input.value == '' || input.value == null){
+      divParent = input.parentElement
+      text = `${input.name} cannot be empty`
+      message.innerText = text
+      divParent.appendChild(message)
+      input.style.outline = '2px solid red'
+      
+    } else {
+      input.style.outline = '1px solid hsl(246, 25%, 77%)'
+    }
+  })
+
 }
 
-function negativeMessage(e, text){
-  let message = document.querySelector('.error-message')
-  message.innerText = text
-  e.target.style.outline = 'solid red 2px'
-}
-
-document
-  .querySelector('#confirm')
-  .addEventListener('click', valideEmail
-  )
+document.querySelector('#confirm').addEventListener('click', validateInfos)
 
 let input = document.querySelectorAll('input')
-
-
